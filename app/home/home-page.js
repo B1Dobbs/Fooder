1991ea8a185666acabc332992aef87bc33a75069
B1Dobbs <brookedobbins5@gmail.com>
2019-03-07 19:57:21 -0600

    Moved building card logic to home-page.js

--- app/home/home-page.js
+++ app/home/home-page.js
@@ -1,11 +1,77 @@
 const topmost = require("ui/frame").topmost;
 var SwipeEvent = require("nativescript-swipe-card");
 var HomeViewModel = require("./home-view-model");
+var StackLayout = require("ui/layouts/stack-layout").StackLayout;
+const AbsoluteLayout = require("tns-core-modules/ui/layouts/absolute-layout").AbsoluteLayout;
+var Label = require("tns-core-modules/ui/label").Label;
+var ImageModule = require("tns-core-modules/ui/image");
 
 
 exports.onNavigatingTo = function(args) {
   const page = args.object;
   page.bindingContext = new HomeViewModel(page.navigationContext);
   const binding = page.bindingContext;
-  binding.getMatches();
+  exports.getMatches(page);
 };
+
+exports.getMatches = function(page) {
+  console.log("getting matches")
+            
+  var cards = [];
+  
+  //Google Places API request here:
+  // return fetch("url string goes here")
+  //     .then(handleErrors)
+  //     .then(function(response) {
+  //         return response.json();
+  //     }).then(function(data) {
+      //build card - this needs to loop through data.petfinder.pets
+      //var arrayLength = data.places.length;
+      var arrayLength = 3;
+      console.log(arrayLength)
+      //for (var i = 0; i < arrayLength; i++) {
+          var border = new StackLayout();
+          var imageContainer = new AbsoluteLayout();
+          var stack = new StackLayout();
+          var image = new ImageModule.Image();
+          var nameLabel = new Label();
+          var descriptionLabel = new Label();
+          image.src="~/images/JasonsDeli.jpg";
+          nameLabel.text = "McDonalds";
+          nameLabel.class = "quicksandBoldLarge";
+          nameLabel.horizontalAlignment="center";
+          descriptionLabel.text = "Fast food";
+          descriptionLabel.textWrap = true;
+          descriptionLabel.class="quicksand";
+          descriptionLabel.alignment="top";
+          descriptionLabel.horizontalAlignment="center";
+          
+          image.stretch = "aspectFill";
+          // imageContainer.addChild(image);
+          // GridLayout.setRow(image,0);
+          // GridLayout.setRow(nameLabel,0);
+          // const firstRow = new ItemSpec(100,"star");
+          // imageContainer.addRow(firstRow);
+
+          stack.addChild(image);
+          stack.addChild(nameLabel);
+          stack.addChild(descriptionLabel);
+          border.addChild(stack);
+          stack.class = "cardStyle";
+          //stack.addChild(image);
+          //stack.addChild(descriptionLabel);
+          
+          cards.push(border);
+          
+     // }
+      page.bindingContext.foodCards = cards;
+ // });
+};
+
+function handleErrors(response) {
+  if (!response.ok) {
+      console.log(JSON.stringify(response));
+      throw Error(response.statusText);
+  }
+  return response;
+}
--- app/home/home-view-model.js
+++ app/home/home-view-model.js
@@ -1,84 +1,14 @@
 const observableModule = require("data/observable");
-var StackLayout = require("ui/layouts/stack-layout").StackLayout;
-const AbsoluteLayout = require("tns-core-modules/ui/layouts/absolute-layout").AbsoluteLayout;
-var Label = require("tns-core-modules/ui/label").Label;
-var ImageModule = require("tns-core-modules/ui/image");
 var LayoutBase = require("tns-core-modules/ui/layouts/layout-base");
 
-function HomeViewModel(petModel) {
+function HomeViewModel() {
     const viewModel = observableModule.fromObject({
         
-        pet: petModel,
-        
-        petCards: Array<LayoutBase>=[],
-        
-        getMatches : function() {
-
-            console.log("getting matches")
-            
-            var cards = [];
-            
-            //Google Places API request here:
-            // return fetch("url string goes here")
-            //     .then(handleErrors)
-            //     .then(function(response) {
-            //         return response.json();
-            //     }).then(function(data) {
-                //build card - this needs to loop through data.petfinder.pets
-                //var arrayLength = data.places.length;
-                var arrayLength = 3;
-                console.log(arrayLength)
-                //for (var i = 0; i < arrayLength; i++) {
-                    var border = new StackLayout();
-                    var imageContainer = new AbsoluteLayout();
-                    var stack = new StackLayout();
-                    var image = new ImageModule.Image();
-                    var nameLabel = new Label();
-                    var descriptionLabel = new Label();
-                    image.src="~/images/JasonsDeli.jpg";
-                    nameLabel.text = "McDonalds";
-                    nameLabel.class = "quicksandBoldLarge";
-                    nameLabel.horizontalAlignment="center";
-                    descriptionLabel.text = "Fast food";
-                    descriptionLabel.textWrap = true;
-                    descriptionLabel.class="quicksand";
-                    descriptionLabel.alignment="top";
-                    descriptionLabel.horizontalAlignment="center";
-                    
-                    image.stretch = "aspectFill";
-                    // imageContainer.addChild(image);
-                    // GridLayout.setRow(image,0);
-                    // GridLayout.setRow(nameLabel,0);
-                    // const firstRow = new ItemSpec(100,"star");
-                    // imageContainer.addRow(firstRow);
-
-                    stack.addChild(image);
-                    stack.addChild(nameLabel);
-                    stack.addChild(descriptionLabel);
-                    border.addChild(stack);
-                    stack.class = "cardStyle";
-                    //stack.addChild(image);
-                    //stack.addChild(descriptionLabel);
-                    
-                    cards.push(border);
-                    
-               // }
-                viewModel.set('petCards',cards)
-           // });
-            
-        }
-    });
-   
+        foodCards: Array<LayoutBase>=[],
     
+    });
+     
     return viewModel;
-}
-
-function handleErrors(response) {
-    if (!response.ok) {
-        console.log(JSON.stringify(response));
-        throw Error(response.statusText);
-    }
-    return response;
 }
 
 module.exports = HomeViewModel;
